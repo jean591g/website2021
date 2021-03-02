2e5591511a1e6c1523e29fee5522447a805880f3
jean591g <jean591g@stud.kea.dk>
2021-03-02 15:29:12 +0100

    Initial commit

---
+++ src/main/java/com/babonneau/website2021/Website2021Application.java
@@ -0,0 +1,13 @@
+package com.babonneau.website2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Website2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Website2021Application.class, args);
+    }
+
+}
